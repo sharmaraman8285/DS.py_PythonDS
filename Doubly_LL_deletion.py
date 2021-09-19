95950e3c04bd3353a3243585b86bee2bdd1c5536
Raman Sharma <69623096+sharmaraman8285@users.noreply.github.com>
2021-09-19 23:36:11 +0530

    Add files via upload

---
+++ Doubly_LL_deletion.py
@@ -0,0 +1,194 @@
+'''
+Doubly Linked List : each node apart from storing its data has two links. The first link points to the previous node in the list and the 
+second link points to the next node in the list.
+
+#Doubly Linked List :-> Deletion
+'''
+
+
+
+# initializing node of a linked list :---------
+
+class Node:     #created node.
+    def __init__(self,data):  #node data legi.
+        self.data=data  # data liya.
+        self.nref=None  # nref = next node refrence start me null hoga.
+        self.pref=None #pref = previous node refrence bhi null hoga.
+class DoublyLL:  # doubly linked list initialize kri.
+    def __init__(self):  
+        self.head=None  #start me head null hoga.
+
+# traversal of a node in forward direction:------------
+
+    def printDLL(self):    #same as linked list.
+        if self.head is None:
+            print("linked list is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data,"<===>",end=" ")
+                n=n.nref
+            if n is None:
+                print("NULL")
+
+# traversal of a node in backward direction:------------
+
+    def printDLL_reverse(self):   #  reverse me krega.
+        print()
+        if self.head is None:  #agar none h to 
+            print("linked list is empty")  #empty print kra dega.
+        else:   #varna
+            n = self.head  #n ko head banaya.
+            while n.nref is not None:  #n ka ref none h ya nhi check krega ye.
+                n=n.nref   #n= badhata rhega.
+            while n is not None:   #jb tak n none ni h.
+                print(n.data,"<===>",end=" ")  #data print kra dega previou node ka or
+                n=n.pref   #ye preivous node pr point krta rhega.
+            
+
+#inserting in a empty node:---------
+
+    def insert_empty(self,data):  
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+        else:
+            print("Linked List is not empty!")
+
+#inserting at begin of a node:---------
+
+    def add_begin(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.nref = self.head
+            self.head.pref = new_node
+            self.head = new_node
+
+#inserting at end of a node:---------
+
+    def add_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.nref is not None:
+                n = n.nref
+            n.nref = new_node
+            new_node.pref = n
+
+#inserting after of a given node:---------
+
+
+    def add_after(self,data,x):
+        if self.head is None:
+            print("LL is empty!")
+        else:
+            n =self.head
+            while n is not None:
+                if x==n.data:
+                    break
+                n = n.nref
+            if n is None:
+                print("Given Node is not present in DLL")
+            else:
+                new_node = Node(data)
+                new_node.nref = n.nref
+                new_node.pref = n
+                if n.nref is not None:
+                    n.nref.pref = new_node
+                n.nref = new_node
+                
+#inserting before of a given node:---------
+
+    def add_before(self,data,x):
+        if self.head is None:
+            print("LL is empty!")
+        else:
+            n = self.head
+            while n is not None:
+                if x==n.data:
+                    break
+                n = n.nref
+            if n is None:
+                print("Given Node is not present in DLL")
+            else:
+                new_node = Node(data)
+                new_node.nref = n
+                new_node.pref = n.pref                
+                if n.pref is not None:
+                    n.pref.nref = new_node
+                else:
+                    self.head = new_node
+                n.pref = new_node
+
+#Deletion from begin:---------
+
+    def delete_begin(self):
+        if self.head is None:
+            print("DLL is empty can't delte !")
+            return
+        if self.head.nref is None:
+            self.head = None
+            print("DLL is empty after deleting the node!")
+        else:
+            self.head = self.head.nref
+            self.head.pref = None
+            
+#Deletion from end:---------
+
+
+    def delete_end(self):
+        if self.head is None:
+            print("DLL is empty can't delte !")
+            return
+        if self.head.nref is None:
+            self.head = None
+            print("DLL is empty after deleting the node!")
+        else:
+            n = self.head
+            while n.nref is not None:
+                n = n.nref
+            n.pref.nref = None
+
+#Deletion by value:---------
+
+
+    def delete_by_value(self,x):
+        if self.head is None:
+            print("DLL is empty can't delte !")
+            return
+        if self.head.nref is None:
+            if x==self.head.data:
+                self.head = None
+            else:
+                print("x is not present in DLL")
+            return
+        if self.head.data == x:
+            self.head = self.head.nref
+            self.head.pref = None
+            return
+        n = self.head
+        while n.nref is not None:
+            if x==n.data:
+                break
+            n = n.nref
+        if n.nref is not None:
+            n.nref.pref = n.pref
+            n.pref.nref = n.nref
+        else:
+            if n.data==x:
+                n.pref.nref = None
+            else:
+                print("x is not present in dll!")
+
+DLL=DoublyLL()
+DLL.add_begin(5)
+DLL.add_begin(7)
+DLL.add_after(8,5)
+DLL.delete_by_value(8)
+DLL.delete_begin()
+DLL.printDLL()
+# DLL.printDLL_reverse()
